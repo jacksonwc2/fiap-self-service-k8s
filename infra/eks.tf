f5ffbb99236d0ec99db07508394410188b9ec7bf
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-23 18:07:44 -0300

    feat: edit security group

--- infra/eks.tf
+++ infra/eks.tf
@@ -8,6 +8,7 @@ resource "aws_eks_cluster" "k8s_cluster" {
 
   vpc_config {
     subnet_ids = concat(module.vpc.private_subnets, module.vpc.public_subnets)
+    security_group_ids = [aws_security_group.eks_security_group.id]
   }
 
   tags = {
@@ -23,7 +24,7 @@ resource "aws_eks_node_group" "k8s_node_group" {
   cluster_name    = var.cluster_name
   node_group_name = "k8s_node_group"
   node_role_arn   = var.iam_role_arn
-  subnet_ids      = concat(module.vpc.private_subnets, module.vpc.public_subnets)
+  subnet_ids      = module.vpc.private_subnets
 
   scaling_config {
     desired_size = 1
